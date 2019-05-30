9005cb8a6f55ebf07ae510e864acbe14dfa116da
Stephanie Keller <stephani.kell@gmail.com>
2019-05-29 22:10:54 -0400

    setup scss, file structure, andbegin app styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import ProductsContainer from './ProductsContainer'
-import CartContainer from './CartContainer'
+import React from 'react';
+import ProductsContainer from './ProductsContainer';
+import CartContainer from './CartContainer';
+import { ReactComponent as Cart } from '../icons/cart.svg';
+import '../styles/containers/app.scss';
 
 const App = () => (
-  <div>
-    <h2>Shopping Cart Example</h2>
+  <div className='app' >
+    <header>
+      <h2>Acme Store</h2>
+      <span className='header-info'>
+        <Cart className='cartIcon--header' />
+        <h6>Your cart is empty</h6>
+      </span>
+    </header>
     <hr/>
     <ProductsContainer />
     <hr/>
